c3d1cda9c934718903f3cfc663176a21b89213e1
Pingsheng Li <52305040+Pingsheng-Kevin@users.noreply.github.com>
2022-02-15 21:54:00 -0500

    Update FBCCA_IT.m

--- CCA_analysis_matlab/FBCCA_IT.m
+++ CCA_analysis_matlab/FBCCA_IT.m
@@ -4,7 +4,8 @@ sum=0;
 for i=1:numFB
     [beta, alpha] = cheby1(FiltOrder, 1, [(lowerFrequency*2)/sampleRate (upperFrequency*2)/sampleRate], 'bandpass');
     FB = filtfilt(beta, alpha, Signal);
-    rho = standardCCA_ITCCA(FB, fundFrequency, numHarmonics, Template);
+    % remove last 4 time points
+    rho = standardCCA_ITCCA(FB(:, :end-4), fundFrequency, numHarmonics, Template);
     sum=sum+(power(i,-a)+b)*(rho^2);
 end
 r=sum;
